3cf3a3517b0cb92832fe0d565065a685087c24df
emabondi <emabond69@gmail.com>
2023-01-01 17:04:41 +0100

    ex00

---
+++ CPP_07/ex00/Whatever.hpp
@@ -0,0 +1,27 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T> void swap(T &x, T &y){
+	T temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T> T min(T x, T y){
+	if (y <= x)
+		return y;
+	else
+		return x;
+}
+
+template <typename T> T max(T x, T y){
+	if (y >= x)
+		return y;
+	else
+		return x;
+}
+#endif
