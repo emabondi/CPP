14b2b3e9b9f5cb9bb9f389f37c4e0bc7bda08960
emabondi <emabond69@gmail.com>
2023-01-08 21:10:13 +0100

    ex03

--- CPP_04/ex03/AMateria.cpp
+++ CPP_04/ex03/AMateria.cpp
@@ -25,4 +25,8 @@ AMateria::AMateria(std::string const & type) : _type(type){
 std::string	const & AMateria::getType() const{
 	return (this->_type);
 }
+
+void AMateria::use(ICharacter& target){
+	(void) target;
+}
  
