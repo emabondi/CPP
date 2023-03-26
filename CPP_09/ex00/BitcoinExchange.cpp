464fdff98362143f173ef96eaa58033db7a9d8ea
emabondi <emabond69@gmail.com>
2023-03-27 01:29:46 +0200

    check leaks

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -1,4 +1,34 @@
 #include "BitcoinExchange.hpp"
+bool	check_date(std::string date){
+	int	count = 0;
+
+	for (std::string::size_type i=0; i < date.size(); i++)
+		if (date[i] == '-')
+			count++;
+	if (count != 2){
+		std::cout << "Error: date => " << date << std::endl;
+		return false;}
+	int first = date.find('-');
+	int	year = atoi(date.substr(0, first).c_str());
+	int second = date.find('-', first + 1);
+	int	month = atoi(date.substr(first + 1, second ).c_str());
+	int	day = atoi(date.substr(second + 1).c_str());
+	if (year < 0){
+		std::cout << "Error: year => " << year << std::endl;
+		return false;}
+	if (month < 1 || month > 12 || day < 1 ||
+			(((month == 11 || month == 4 || month == 6 || month == 9) && day > 30) ||
+			((month == 1 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 31))){
+		std::cout << "Error: month or day => " << month << "-" << day << std::endl;
+		return false;}
+	if (month == 2 && year % 4 == 0 && day > 29)
+		std::cout << "Error: february day => " << date << std::endl;
+	else if (month == 2 && day > 28)
+		std::cout << "Error: february day => " << date << std::endl;
+		//std::cout << "Year:" << year << " month: " << month << " day: " << day << std::endl;
+
+	return true;
+}
 
 bool	check_value(std::string value){
 	char	*endptr;
@@ -49,8 +79,8 @@ void	check_exchange(char *f, std::map<std::string, float> map)
 		}
 		date = line.substr(0, pos - 1);
 		svalue = line.substr(pos + 2);
-		//check date
-		if(!check_value(svalue))
+		
+		if(!check_value(svalue) || !check_date(date))
 			continue ;
 		value = atof(svalue.c_str());
 		if (map.count(date))
